f8ac7724c2f624a606b20e85febe15eebff1d5dc
sayanbose-100 <bose.sayan2512000@gmail.com>
2023-12-30 10:24:25 +0530

    A minor change in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ function App() {
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
-    // passwordRef.current?.setSelectionRange(0, 3);
 
     window.navigator.clipboard.writeText(password);
   }, [password]);
